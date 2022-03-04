2450753849e2295f19e0d175fba31e63f0a19ed8
Rik Shaw <rikshaw76@gmail.com>
2022-03-04 10:18:24 -0600

    use loginctl to detect dm and de

--- wasta-multidesktop/scripts/set-session-env.sh
+++ wasta-multidesktop/scripts/set-session-env.sh
@@ -73,17 +73,17 @@ script_exit() {
 # ------------------------------------------------------------------------------
 
 # Determine display manager.
-curr_dm=$(journalctl -b 0 | grep -i "New session .* of user lightdm\|New session .* of user gdm" | tail -n 1 | sed 's@^.*New session .* of user \(.*\)\.@\1@')
+#curr_dm=$(journalctl -b 0 | grep -i "New session .* of user lightdm\|New session .* of user gdm" | tail -n 1 | sed 's@^.*New session .* of user \(.*\)\.@\1@')
 # Get rid of 2nd parenthesis.
-if [[ $(echo $SUPPORTED_DMS | grep -w $curr_dm) ]]; then
-    CURR_DM=$curr_dm
-else
+#if [[ $(echo $SUPPORTED_DMS | grep -w $curr_dm) ]]; then
+#    CURR_DM=$curr_dm
+#else
     # Unsupported display manager!
-    log_msg "$(date)"
-    log_msg "Error: Display manager \"$curr_dm\" not supported."
+#    log_msg "$(date)"
+#    log_msg "Error: Display manager \"$curr_dm\" not supported."
     # Exit with code 0 so that login can continue.
-    script_exit 0
-fi
+#    script_exit 0
+#fi
 
 # 2022-01-17 rik: 22.04 gdm/lightdm logging reference:
 # gdm creates session with c# for gdm and # only for REAL USER, e.g.:
@@ -102,14 +102,22 @@ touch $CURR_SESSION_FILE
 PREV_SESSION_FILE="${LOGDIR}/$CURR_USER-prev-session"
 PREV_SESSION=$(cat $PREV_SESSION_FILE)
 
+CURR_SESSION_ID=$(loginctl show-user $CURR_UID | grep Display= | sed s/Display=//)
+if ! [ $CURR_SESSION_ID ]; then
+    # NO display, so not graphical and don't continue
+    exit 0
+fi
+CURR_SESSION=$(loginctl show-session $CURR_SESSION_ID | grep Desktop= | sed s/Desktop=//)
+CURR_DM=$(loginctl show-session $CURR_SESSION_ID | grep Service= | sed s/Service=//)
+
 # Get current user and session name (can't depend on full env at login).
 if [[ $CURR_DM == 'gdm' ]]; then
 
+    log_msg "GDM detected"
     # TODO: Need a different way to verify wayland session.
-    CURR_SESSION=$(journalctl -b 0 | grep "setting DESKTOP_SESSION=" | tail -n 1 | sed 's@^.*DESKTOP_SESSION=@@')
+#    CURR_SESSION=$(journalctl -b 0 | grep "setting DESKTOP_SESSION=" | tail -n 1 | sed 's@^.*DESKTOP_SESSION=@@')
     # X: ubuntu-xorg
     # Way: ubuntu-wayland??
-
 
     # X:
     # grep "setting DESKTOP_SESSION=" | tail -n 1 | sed 's@^.*DESKTOP_SESSION=@@'
@@ -132,16 +140,16 @@ elif [[ $CURR_DM == 'lightdm' ]]; then
 
     # since running at login and logout, need different way to detect
     # session since logs could have rotated resulting in undetected session
-    CURR_SESSION=$(grep -a "Greeter requests session" /var/log/lightdm/lightdm.log | \
-        tail -1 | sed 's@.*Greeter requests session \(.*\)@\1@')
+#    CURR_SESSION=$(grep -a "Greeter requests session" /var/log/lightdm/lightdm.log | \
+#        tail -1 | sed 's@.*Greeter requests session \(.*\)@\1@')
 
-    if ! [[ "$CURR_SESSION" ]]; then
+#    if ! [[ "$CURR_SESSION" ]]; then
         # Get the user's saved current session (deleted by wasta-logout)
-        if [[ "$(cat $CURR_SESSION_FILE)" ]]; then
-            log_msg "CURR_SESSION not detected, loading from CURR_SESSION_FILE:$(cat $CURR_SESSION_FILE)"
-            CURR_SESSION=$(cat $CURR_SESSION_FILE)
-        fi
-    fi
+#        if [[ "$(cat $CURR_SESSION_FILE)" ]]; then
+#            log_msg "CURR_SESSION not detected, loading from CURR_SESSION_FILE:$(cat $CURR_SESSION_FILE)"
+#            CURR_SESSION=$(cat $CURR_SESSION_FILE)
+#        fi
+#    fi
 fi
 
 # SAVE user's current session
